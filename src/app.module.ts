faa43098061713ba07c659f3556130be0425da1d
Jakub Dolata <jdolata5@gmail.com>
2023-07-09 11:57:43 +0200

    add airtable service and env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ActionsModule } from './actions/actions.module';
 
 @Module({
-  imports: [ActionsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ActionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/core/services/airtable/airtable.service.ts
+++ src/core/services/airtable/airtable.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import Airtable from 'airtable';
 
-@Injectable()
-export class AirtableService {}
+Airtable.configure({
+  endpointUrl: process.env.DATABASE_URL,
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+
+export default Airtable.base(process.env.AIRTABLE_BASE);
